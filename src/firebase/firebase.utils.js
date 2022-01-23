de0bfbe514ad7b775861feba75ace893b27e8b60
Ramin <fnu_ramin@yahoo.com>
2022-01-23 01:19:12 -0500

    added the sign-up functionality and initialized it with firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,7 +10,33 @@ const config = {
     messagingSenderId: "721355841937",
     appId: "1:721355841937:web:56f978153b8d8c766dfa30",
     measurementId: "G-GVDTS9C2XX"
-  }
+  };
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot =  await userRef.get();
+
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt =  new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+
+      } catch (error){
+        console.log('error creating user', error.message)
+      }
+    }
+
+    return userRef;
+  };
 
   firebase.initializeApp(config);
 
